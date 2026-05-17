d853f9d262274af2ec90a4381e640046c75bd646
SSNC-Studio <ssnc.studios@gmail.com>
2026-05-17 20:38:45 +0000

    First real test run

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Services--VersionsAPI
-This is a repository containing all Version Information about SSNC Services (loading screens, certain scripts, etc.). This should be updated as SSNC Services update.
+# SSNCServices--VersionsAPI
+This is a repository containing all Version Information about SSNC Services (loading screens, certain scripts, etc.). This should be updated as SSNC Services update. This will contain every single version of a Service that is published, and meant to be viewed by the public.
--- index.js
+++ index.js
@@ -12,11 +12,21 @@ app.use(express.json());
 app.get("/api/script/GLOBAL-LOADING-SCREEN", (req, res) => {
     res.json({
         version: "1.0.1",
-        author: "[SSNC] SPHLAC",
-        contributors: {
-            Nobody: ""
-        },
-        totalLines: 1462
+        author: "[FOUNDER] SPHLAC",
+        contributors: "[FOUNDER] camaleon45678., [DEV] Hugo",
+        compileDate: "17-05-2026 (dd/mm/yyyy)",
+        allowPreviousVersions: false,
+        DEPRECATED: false,
     });
 });
+
+app.get("/api/script/GLOBAL-LOADING-SCREEN/1.0.0", (req, res) => {
+    res.json({
+        version: "1.0.0",
+        author: "[FOUNDER] SPHLAC",
+        contributors: "(Nobody)",
+        compileDate: "13-05-2026 (dd/mm/yyyy)",
+        DEPRECATED: true,
+    });
+})
 
